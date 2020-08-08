72c09107dd638a0c81c40204d376782f9f6b8b69
Rodrigo Marcondes <rodrigomarcondes2000@gmail.com>
2020-08-07 21:40:27 -0300

    Finishing user and tasks CRUD APIs

---
+++ src/routers/task.js
@@ -0,0 +1,82 @@
+const express = require('express')
+const Task = require('../models/task')
+
+const router = new express.Router()
+
+router.post('/tasks', async (req, res) => {
+    const task = new Task(req.body)
+
+    try {
+        await task.save()
+        res.status(201).send(task)
+    } catch(e) {
+        res.status(400).send(e)
+    }
+})
+
+router.get('/tasks', async (req, res) => {
+    try {
+        const tasks = await Task.find({})
+        res.send(tasks)
+    } catch(e) {
+        res.status(500).send(error)
+    }
+})
+
+router.get('/tasks/:id', async (req, res) => {
+    const { id } = req.params
+
+    try {
+        const task = await Task.findById(id)
+
+        if (!task) {
+            return res.status(404).send()
+        }
+
+        return res.send(task)
+    } catch (e) {
+        res.status(500).send(e)
+    }
+})
+
+router.patch('/tasks/:id', async (req, res) => {
+    const { id } = req.params;
+    const allowedUpdates = ['description', 'completed']
+
+    const updates = Object.keys(req.body)
+    const isValidOperation = updates.every(update => allowedUpdates.includes(update))
+
+    if (!isValidOperation) {
+        return res.status(400).send({ error: 'Invalid updates!' })
+    }
+
+    try {
+        const task = await Task.findByIdAndUpdate(id, req.body, { new: true, runValidators: true })
+
+        if (!task) {
+            return res.status(404).send()
+        }
+
+        res.send(task)
+    } catch(e) {
+        return res.status(400).send(e)
+    }
+})
+
+router.delete('/tasks/:id', async (req, res) => {
+    const { id } = req.params
+
+    try {
+        const task = await Task.findByIdAndDelete(id)
+
+        if (!task) {
+            return res.status(404).send()
+        }
+
+        res.send(task)
+    } catch(e) {
+        res.status(400).send(e)
+    }
+})
+
+module.exports = router
